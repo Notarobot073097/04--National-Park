58d27a4733da62c7942fd128f1c1cae23e56b3da
Notarobot073097 <viewtifulkobey@yahoo.com>
2023-02-09 14:44:39 -0700

    Added functioning hamburg menu with JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 //Inject current year into footer
 const correctYear = new Date();
 document.querySelector('#year').textContent = correctYear.getFullYear();
+
+function menuToggle(){
+document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgMenu");
+
+x.onclick = menuToggle;
